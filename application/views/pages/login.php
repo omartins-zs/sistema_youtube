6319c35373610d37c0981839d4e4c4f374233bcc
omartins-zs <gabrielmatheus209@gmail.com>
2023-02-02 14:31:16 -0300

    Criando page de Login e Estilizando e traduzindo

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -27,7 +27,71 @@
 </head>
 
 <body class="hold-transition login-page">
+	<div class="login-box">
+		<div class="login-logo">
+			<a href="../../index2.html"><b>Sistema</b>YouTube</a>
+		</div>
+		<!-- /.login-logo -->
+		<div class="login-box-body">
+			<p class="login-box-msg">Entre para iniciar sua sessão</p>
 
+			<form action="../../index2.html" method="post">
+				<div class="form-group has-feedback">
+					<input type="email" id="password" class="form-control" placeholder="E-mail">
+					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+				</div>
+				<div class="form-group has-feedback">
+					<input type="password" id="password" class="form-control" placeholder="Senha">
+					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				</div>
+				<div class="row">
+					<div class="col-xs-8">
+						<div class="checkbox icheck">
+							<label>
+								<input type="checkbox"> Permanecer conectado
+							</label>
+						</div>
+					</div>
+					<!-- /.col -->
+					<div class="col-xs-4">
+						<button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+					</div>
+					<!-- /.col -->
+				</div>
+			</form>
+
+			<div class="social-auth-links text-center">
+				<p>- OR -</p>
+				<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+					Facebook</a>
+				<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+					Google+</a>
+			</div>
+			<!-- /.social-auth-links -->
+
+			<a href="#">Esqueceu a senha</a><br>
+			<a href="register.html" class="text-center">Quero me cadastrar</a>
+
+		</div>
+		<!-- /.login-box-body -->
+	</div>
+	<!-- /.login-box -->
+
+	<!-- jQuery 2.2.0 -->
+	<script src="assets/js/jQuery/jQuery-2.2.0.min.js"></script>
+	<!-- Bootstrap 3.3.6 -->
+	<script src="assets/js/bootstrap/bootstrap.min.js"></script>
+	<!-- iCheck -->
+	<script src="assets/js/iCheck/icheck.min.js"></script>
+	<script>
+		$(function() {
+			$('input').iCheck({
+				checkboxClass: 'icheckbox_square-blue',
+				radioClass: 'iradio_square-blue',
+				increaseArea: '20%' // optional
+			});
+		});
+	</script>
 </body>
 
 </html>
