42c0238586c46adb6b12c8638722cdc9dd39e76b
omartins-zs <gabrielmatheus209@gmail.com>
2023-02-02 14:33:04 -0300

    Criando controller de Login

---
+++ application/controllers/Login.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+		$this->load->helper('form');
+		date_default_timezone_set('America/Sao_Paulo');
+	}
+
+	public function index()
+	{
+		$this->load->helper(array('form'));
+		$this->load->view('pages/login');
+	}
+}
