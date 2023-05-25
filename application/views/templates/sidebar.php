b83ee7a527427f8ff054dd0390a112a85c2152bf
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-25 11:17:08 -0300

    Ajuste na Imagem e Add Nome do Usuario Logado

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,3 +1,8 @@
+<?php
+$session_youtube = $this->session->userdata('logged_in');
+$nomeUsuario = $session_youtube['nomeUsuario'];
+?>
+
 <!-- Left side column. contains the logo and sizdebar -->
 <aside class="main-sidebar">
 	<!-- sidebar: style can be found in sidebar.less -->
@@ -5,10 +10,10 @@
 		<!-- Sidebar user panel -->
 		<div class="user-panel">
 			<div class="pull-left image">
-				<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+				<img src="<?= base_url(); ?>assets/img/01avatar.png" class="img-circle" alt="User Image">
 			</div>
 			<div class="pull-left info">
-				<p>Alexander Pierce</p>
+				<p><?= $nomeUsuario; ?></p>
 				<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
 			</div>
 		</div>
