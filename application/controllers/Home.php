c5e484ca480a25b467dbbec0d7a0c5052a06e9f5
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-30 08:41:41 -0300

    Add Function para Cadastro dos Usuarios

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -47,4 +47,51 @@ class Home extends CI_Controller
 			redirect('login', 'refresh');
 		}
 	}
+
+	/*
+    * USUARIOS
+    */
+	public function cadastrausuario()
+	{
+		// if ($this->session->userdata('logged_in')) { // VALIDA USUARIO LOGADO
+		$this->load->model('Perfil_model');
+		$resultadoPerfil = $this->Perfil_model->buscaPerfil();
+		$dados['resultadoPerfil'] = $resultadoPerfil;
+
+		if ($this->input->post()) {
+			if ((!empty(trim($this->input->post('nome')))) || (!empty(trim($this->input->post('login')))) || (!empty(trim($this->input->post('email')))) || (!empty(trim($this->input->post('senha')))) || (!empty(trim($this->input->post('perfilid'))))) {
+
+				$dadosusuario['nome'] = $this->input->post('nome');
+				$dadosusuario['login'] = $this->input->post('login');
+				$dadosusuario['email'] = $this->input->post('email');
+				$dadosusuario['senha'] = $this->input->post('senha');
+				$dadosusuario['datacadastro'] = date('Y-m-d');
+				$dadosusuario['perfilid'] = $this->input->post('perfilid');
+				$dadosusuario['status'] = 1;
+
+				$this->load->model('Usuario_model');
+				$resultadocadastrousuario = $this->Usuario_model->cadastrausuario($dadosusuario);
+
+				if ($resultadocadastrousuario) {
+					$dados['telaativa'] = 'usuarios';
+					$dados['tela'] = 'dashboard';
+				} else {
+					$dados['telaativa'] = 'usuarios';
+					$dados['msg'] = 'Ocorreu um erro ao cadastrar o usuario! Atualize a pagina e tente novamente';
+					$dados['tela'] = 'usuarios/cadastro_usuario';
+				}
+				$this->load->view('pages/home', $dados);
+			} else {
+				$dados['telaativa'] = 'usuarios';
+				$dados['msg'] = 'Dados Imcompletos! Preencha os dados e tente novamente';
+				$dados['tela'] = 'usuarios/cadastro_usuario';
+				$this->load->view('pages/home', $dados);
+			}
+		} else {
+			$dados['telaativa'] = 'usuarios';
+			$dados['tela'] = 'usuarios/cadastro_usuario';
+			$this->load->view('pages/home', $dados);
+		}
+		// }
+	}
 }
