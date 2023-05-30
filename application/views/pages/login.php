f7b414209f18f5d6e5109fb769efd1231bcffff2
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-30 11:00:50 -0300

    Ajuste no Form e Links de Importaçao CSS e JS

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -4,19 +4,19 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>AdminLTE 2 | Log in</title>
+	<title>Sistema Youtube 2 | Login</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<!-- Bootstrap 3.3.6 -->
-	<link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap/bootstrap.min.css">
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/font-awesome.min.css">
 	<!-- Ionicons -->
-	<link rel="stylesheet" href="assets/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/ionicons.min.css">
 	<!-- Theme style -->
-	<link rel="stylesheet" href="assets/css/Adminlte/AdminLTE.min.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/Adminlte/AdminLTE.min.css">
 	<!-- iCheck -->
-	<link rel="stylesheet" href="assets/css/plugins/iCheck/square/blue.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/plugins/iCheck/square/blue.css">
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -36,41 +36,39 @@
 			<p class="login-box-msg">Entre para iniciar sua sessão</p>
 
 			<?php
-			echo form_open('Autentica');
+			// echo form_open('Autentica');
 			echo validation_errors();
 			?>
-			<div class="form-group has-feedback">
-				<input type="email" name="email" id="email" class="form-control" placeholder="E-mail ou Usuário">
-				<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-			</div>
-			<div class="form-group has-feedback">
-				<input type="password" name="password" id="password" class="form-control" placeholder="Senha">
-				<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-			</div>
-			<div class="row">
-				<div class="col-xs-8">
-					<div class="checkbox icheck">
-						<label>
-							<input type="checkbox"> Permanecer conectado
-						</label>
+			<form action="<?= base_url() ?>Autentica" method="post">
+				<div class="form-group has-feedback">
+					<?php echo form_error('username'); ?>
+					<input name="login" id="login" class="form-control" placeholder="Usuário">
+					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+				</div>
+				<div class="form-group has-feedback">
+					<?php echo form_error('password'); ?>
+					<input type="password" name="senha" id="senha" class="form-control" placeholder="Senha">
+					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				</div>
+				<div class="row">
+					<div class="col-xs-8">
+						<div class="checkbox icheck">
+							<label>
+								<input type="checkbox"> Permanecer conectado
+							</label>
+						</div>
 					</div>
+					<!-- /.col -->
+					<div class="col-xs-4">
+						<button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+					</div>
+					<!-- /.col -->
 				</div>
-				<!-- /.col -->
-				<div class="col-xs-4">
-					<button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
-				</div>
-				<!-- /.col -->
-			</div>
+			</form>
 			<?php
-			echo form_close();
+			// echo form_close();
 			?>
-			<!-- <div class="social-auth-links text-center">
-				<p>- OR -</p>
-				<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-					Facebook</a>
-				<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-					Google+</a>
-			</div> -->
+
 			<!-- /.social-auth-links -->
 
 			<a href="#">Esqueceu a senha</a><br>
@@ -82,11 +80,11 @@
 	<!-- /.login-box -->
 
 	<!-- jQuery 2.2.0 -->
-	<script src="assets/js/jQuery/jQuery-2.2.0.min.js"></script>
+	<script src="<?= base_url(); ?>assets/js/jQuery/jQuery-2.2.0.min.js"></script>
 	<!-- Bootstrap 3.3.6 -->
-	<script src="assets/js/bootstrap/bootstrap.min.js"></script>
+	<script src="<?= base_url(); ?>assets/js/bootstrap/bootstrap.min.js"></script>
 	<!-- iCheck -->
-	<script src="assets/js/iCheck/icheck.min.js"></script>
+	<script src="<?= base_url(); ?>assets/js/iCheck/icheck.min.js"></script>
 	<script>
 		$(function() {
 			$('input').iCheck({
