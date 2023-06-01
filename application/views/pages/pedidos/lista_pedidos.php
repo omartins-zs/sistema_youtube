2f199b8133426fc0c10284051cdea95824deecbb
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-01 11:59:31 -0300

    Criando View de Lista de Pedidos

---
+++ application/views/pages/pedidos/lista_pedidos.php
@@ -0,0 +1,89 @@
+<link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="<?= base_url(); ?>assets/datatables/dataTables.bootstrap.css">
+
+<div class="content-wrapper">
+	<section class="content-header">
+		<h1>Lista de Produtos</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+			<li>Produtos</li>
+			<li class="active">Lista </li>
+		</ol>
+	</section>
+
+	<section class="content">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-lg-12">
+				<div class="box box-warning">
+
+					<?php
+					if (isset($msg)) {
+						echo '<div class="box-header with-border">' . $msg . '</div>';
+					}
+					?>
+					<div class="box">
+						<div class="box-header with-border">
+							<h3 class="box-title">Lista de Produtos</h3>
+						</div>
+						<!-- /.box-header -->
+						<div class="box-body">
+							<table id="example1" class="table table-bordered table-striped">
+								<thead>
+									<tr>
+										<th style="width:24px">&nbsp;</th>
+										<th>Nome do Produto</th>
+										<th>E-mail</th>
+										<th>Cadastrado em:</th>
+										<th>Ultimo Acesso</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+									if (isset($resultadoProduto)) {
+										foreach ($resultadoProduto as $produtos) {
+									?>
+											<tr>
+												<td><a href="consultausuario?id=<?= $usuarios->id; ?>"><i class="fa fa-share"></i></a></td>
+												<td><?= $clientes->nome; ?></td>
+												<td><?= $produtos->email; ?></td>
+												<td><?= $produtos->datacadastro; ?></td>
+												<td><?= $produtos->dataultimoacesso; ?></td>
+											</tr>
+									<?php
+										}
+									}
+									?>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<script src="<?= base_url(); ?>assets/js/jquery/jquery-2.2.3.min.js"></script>
+
+<script src="<?= base_url(); ?>assets/js/bootstrap/bootstrap.min.js"></script>
+<script src="<?= base_url(); ?>assets/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url(); ?>assets/datatables/dataTables.bootstrap.min.js"></script>
+
+<!-- page script -->
+<script>
+	var base_url = '<?= base_url() ?>';
+	$(document).ready(function() {
+
+	});
+	$(function() {
+		$('#example1').DataTable({
+			"paging": true,
+			"lengthChange": false,
+			"searching": false,
+			"ordering": true,
+			"info": true,
+			"autoWidth": false
+		});
+	});
+</script>
