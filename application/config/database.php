b82ccfab6c2c19d6f85d89d5bc8af299287503a4
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-02 16:09:46 -0300

    Ajuste para Banco MySQL

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,8 +76,8 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'sistemayoutube',
-	'password' => 'Sistema_Youtube@#24',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'sistemayoutube',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
