6a6b166f8aebcc7c7292b1ab463b989b8eb08b0f
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-30 14:59:30 -0300

    Add Librarie MY_Form_validation

---
+++ application/libraries/MY_Form_validation.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class MY_Form_validation extends CI_Form_validation
+{
+
+	function run($module = '', $group = '')
+	{
+		(is_object($module)) and $this->CI = &$module;
+		return parent::run($group);
+		//if ($this->form_validation->run($this) == FALSE);
+	}
+}
